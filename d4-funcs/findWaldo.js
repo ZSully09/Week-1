40d73077cd932a94e72e9b1af8df1fe90dd72d6c
ZSully09 <zsullivan93@gmail.com>
2019-10-17 18:44:07 +0000

    adding exercise descriptions

--- d4-funcs/findWaldo.js
+++ d4-funcs/findWaldo.js
@@ -1,3 +1,7 @@
+// Exercise 1: Modify the callback function in the previous example so that it accepts a single argument index and logs it. The problem should therefore output something like "Found Waldo at index 2!".
+
+//Exercise 2: Refactor the function findWaldo to use the forEach() method instead of a for loop.
+
 // The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
