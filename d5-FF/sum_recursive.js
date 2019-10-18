ff0ec55aad9816203c9def33b61d0e92c1071b21
ZSully09 <zsullivan93@gmail.com>
2019-10-18 20:15:56 +0000

    Implement the recursive case and test

--- d5-FF/sum_recursive.js
+++ d5-FF/sum_recursive.js
@@ -20,4 +20,14 @@ function sumToOne(n) {
   if (n === 1) {
     return 1;
   }
+  // Imnplement Recursive Case:
+  return n + sumToOne(n - 1);
 }
+// console.log(sumToOne(1));    test 1(confirm returns 1 when n = 1)
+console.log(sumToOne(4));
+console.log(sumToOne(5));
+
+/*
+When n = 4: the result is equivalent to 4 + 3 + 2 + 1 = 10.
+When n = 5: the result is equivalent to 5 + 4 + 3 + 2 + 1 = 15
+*/
