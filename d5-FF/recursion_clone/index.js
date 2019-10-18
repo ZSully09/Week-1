ca72d8c573edd01021c5549a0d1164b09ed53fa3
ZSully09 <zsullivan93@gmail.com>
2019-10-18 21:18:56 +0000

    additional notes and additionally running the recursive base from 7 to 3 rather than 3 to 7

--- d5-FF/recursion_clone/index.js
+++ d5-FF/recursion_clone/index.js
@@ -1,5 +1,5 @@
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  // Sum all the values from fromN up to toN [3 --> 7]
   // Add base Case
   if (fromN === toN) {
     return fromN;
@@ -15,4 +15,20 @@ module.exports = sum;
 /*
 Base Case: fromN === toN
 Recursive Case: fromN + sum(fromN + 1, toN)
+*/
+
+// Sum all the values of toN to fromN [ 7 --> 3]
+
+function sum(fromN, toN) {
+  if (fromN === toN) {
+    return fromN;
+  }
+  return toN + sum(fromN, toN - 1);
+}
+
+console.log(sum(3, 7));
+
+/*
+Base Case: fromN === toN
+Recursive Case: toN + sum(fromN, toN - 1)
 */
