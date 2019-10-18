cfa0df6dad397dfe0d2ae794d5f2145892f5c6b6
ZSully09 <zsullivan93@gmail.com>
2019-10-18 02:09:37 +0000

    mapTest initial input

---
+++ d4-funcs/map_builtin/mapTest.js
@@ -0,0 +1,13 @@
+// Use the map method on lighthouses so that we instead see an array of numbers.
+
+const lighthouses = [
+  "Gibraltar Point",
+  "Peggy's Point",
+  "Cove Island",
+  "Discovery Island",
+  "Cape Scott",
+  "Point Clark",
+  "Kincardine"
+];
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
