163a5053f656d55981b9d2c202f4c3c872afa3b2
ZSully09 <zsullivan93@gmail.com>
2019-11-04 17:59:26 +0000

    programming kata challenge

--- case_maker.js
+++ case_maker.js
@@ -1,7 +1,31 @@
 let camelCase = function(input) {
-  // Your code here
+  const splitInput = input.split(' ');
+  if (splitInput.length === 1) {
+    return splitInput[0].toString();
+  }
+  const array = [];
+  for (let i = 1; i < splitInput.length; i++) {
+    // console.log(splitInput[i]);
+    const first = splitInput[i][0];
+    // console.log(first);
+    // console.log(first.toUpperCase());
+    const newFirst = first.toUpperCase();
+    const rest = splitInput[i].slice(1);
+    // console.log(rest);
+    // console.log(rest.toLowerCase());
+    const newRest = rest.toLowerCase();
+    // console.log(newFirst + newRest);
+    const word = splitInput[0] + newFirst + newRest;
+    // console.log(word);
+    array.push(word);
+  }
+  // console.log(array);
+  const newString = array.join('');
+  // console.log(newArray);
+
+  return newString;
 };
 
-console.log(camelCase('this is a string'));
-console.log(camelCase('loopy lighthouse'));
-console.log(camelCase('supercalifragalisticexpialidocious'));
+console.log(camelCase('this is a string')); // thisIsAString
+console.log(camelCase('loopy lighthouse')); // loopyLighthouse
+console.log(camelCase('supercalifragalisticexpialidocious')); //supercalifragalisticexpialidocious
